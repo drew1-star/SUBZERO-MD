efb7b6fa1b08d9f747a894e3b364af77c6fe6dae
drew1-star <zulex2007@gmail.com>
2025-07-29 15:30:38 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;iREiDbLZ#Fh5HqrTKfJxG05Fcvn46SgkcXNZDjAMczDrvYSQrvhE", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "263785785040", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
